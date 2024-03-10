e30dd82cbabb27cb4aed74121ec3c7feccb00f70
Path1an <odintobiassen@gmail.com>
2024-03-10 14:26:46 +0100

    Python Introduction

---
+++ python_intro/calculator.py
@@ -0,0 +1,11 @@
+#addition
+number1 = float(input("First "))
+number2 = float(input("Second "))
+sum_addition = number1 + number2
+print("sum:", sum_addition)
+
+#Subtraction
+number3 = float(input("First "))
+number4 = float(input("Second "))
+sum_subtraction = number3 - number4
+print("sum:", sum_subtraction)
---
+++ python_intro/index.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+# variables and values
+new_patient = 'John Smith'
+age = 20
+in_hospital = True
+print(new_patient, age)
+
+#input in the terminal
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print(age)
+
+''' Built in function to converting the type of variables:
+int()
+10
+float()
+10.1
+bool()
+True / False
+str()
+"String" '''
