7fd6f43aa9611633113ba3d3ee2502128b66293b
Path1an <odintobiassen@gmail.com>
2024-03-15 17:33:34 +0100

    calculator update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # freetime.github.io
 programming and coding outside of school.
 
-## This repository is where I code and program on my free time. These are my personal projects I work on outside of school or any other official sercumstance.
+## This repository is where I code and program on my free time, to expiriment with new programming languages, or contunue to develop my programming and coding skills. These are my personal projects I work on outside of school or any other official sercumstance.
--- python_intro/calculator.py
+++ python_intro/calculator.py
@@ -1,11 +1,23 @@
 #addition
-number1 = float(input("First "))
-number2 = float(input("Second "))
+number1 = float(input("First addition number "))
+number2 = float(input("Second number "))
 sum_addition = number1 + number2
 print("sum:", sum_addition)
 
 #Subtraction
-number3 = float(input("First "))
-number4 = float(input("Second "))
+number3 = float(input("First subtraction number "))
+number4 = float(input("Second number "))
 sum_subtraction = number3 - number4
 print("sum:", sum_subtraction)
+
+#Multiplication
+number5 = float(input("First multiplication number "))
+number6 = float(input("Second number "))
+sum_multiplication = number5 * number6
+print("sum:", sum_multiplication)
+
+#Division
+number7 = float(input("First division number "))
+number8 = float(input("Second number "))
+sum_division = number7 / number8
+print("sum:", sum_division)
