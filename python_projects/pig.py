ace7b76cdc4b088b1fc5476b0c59cccd9fa0bfc9
Path1an <odintobiassen@gmail.com>
2024-04-12 13:47:22 +0200

    fikset_feil

--- python_projects/pig.py
+++ python_projects/pig.py
@@ -28,21 +28,21 @@ while max(player_scores)< max_score:
         print("\nPlayer number", player_idx  + 1, "turn has started!\n")
         current_score = 0
 
-    while True: 
-        should_roll = input("Would you like to roll (y)? ")
-        if should_roll.lower() != "y":
-            break
+        while True: 
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
 
-        value = roll()
-        if value == 1:
-            print("You rolled a 1! Turn done!")
-            current_score = 0
-            break
-        else:
-            current_score += value
-            print("You rolled a:", value)
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
 
-        print("Your score is:", current_score)
+            print("Your score is:", current_score)
 
-    player_scores[player_idx] += current_score
-    print("Your total score is:", player_scores[player_idx])
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
