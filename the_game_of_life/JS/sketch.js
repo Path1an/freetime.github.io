198d20fe2a208406e4f2c159fc886425f7b9aa74
Path1an <odintobiassen@gmail.com>
2024-06-04 10:41:38 +0200

    fixed
    never mind the huge changed I fixed it.

--- the_game_of_life/JS/sketch.js
+++ the_game_of_life/JS/sketch.js
@@ -1,15 +1,21 @@
 let rows = 20;
 let columns = 20;
 let resolution = 30;
+let grid;
 
-const matrix = Array(rows).fill().map(() =>
-    Array(columns).fill().map(() => Math.round(Math.random()))
-);
+
+function make2DArray (columns, rows){
+    const matrix = Array(rows).fill().map(() =>
+        Array(columns).fill().map(() => Math.round(Math.random()))
+    );
+    return matrix;
+}
 
 function setup() {
     createCanvas(600, 600);
     columns = width / resolution;
     rows = height / resolution;
+    grid = make2DArray(columns, rows);
 }
 
 function draw() {
@@ -18,35 +24,34 @@ function draw() {
         for (let j = 0; j < rows; j++) {
             let x = i * resolution;
             let y = j * resolution;
-            if (matrix [i] [j] == 1){
+            if (grid [i] [j] == 1){
                 fill(255);
-                stroke(255);
+                stroke(69);
                 rect(x, y, resolution -1, resolution -1);
             }
         }
     }
     
-    const matrix = Array(rows).fill().map(() =>
-        Array(columns).fill().map(() => Math.round(Math.random()))
-    );
+    let next = make2DArray(columns, rows);
 
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
            
             let sum = 0;
-            let neighbors = count(matrix, i, j);
+            let neighbors = count(grid, i, j);
         }
     } 
-    
+
+    grid = next;
 }
 
-countNeighbors (matrix, x, y) {
+ function countNeighbors (grid, x, y) {
     let sum = 0;
     for (let i = -1; i < 2; i ++) {
         for (let j = -1; j < 2; j ++) {
-            sum += matrix[i][j];
+            sum += grid[i][j];
         }
     }
-    sum -= matrix[x][y];
+    sum -= grid[x][y];
     return sum;
 }
